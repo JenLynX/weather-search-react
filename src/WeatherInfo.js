89797d4e21492ff37148aa9ec6c65cac36002adc
JenLynX <104281812+JenLynX@users.noreply.github.com>
2022-09-25 13:10:45 -0700

    changed style

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,22 +15,22 @@ export default function WeatherInfo(props) {
               <li>Wind: {Math.round(props.data.wind)} km/h</li>
             </ul>
           </div>
-          <div className="col-2 text-center">
-            <div>
-              <WeatherIcon code={props.data.iconUrl} size={64} />
-            </div>
-          </div>
-          <div className="col-2">
-            {" "}
-            <WeatherTemperature celsius={props.data.temperature} />
-          </div>
-          <div className="col-5 text-center">
+          <div className="col-4 text-center">
             <h5>
               <FormattedDate date={props.data.date} />
             </h5>
             <h1>{props.data.city}</h1>
 
             <h6 className="text-capitalize">{props.data.description}</h6>
+          </div>
+          <div className="col-2 text-left">
+            <div>
+              <WeatherIcon code={props.data.iconUrl} size={64} />
+            </div>
+          </div>
+          <div className="col-2 text-left">
+            {" "}
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
       </div>
